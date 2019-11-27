4ed89dbb3c9349a589858c5448753ce00882eaa5
Alexandros Kritikos <akritikos@outlook.com>
2019-11-28 01:20:14 +0200

    :card_file_box: Persists IP to database

--- src/NoviSample.Persistence/Migrations/20191127231654_AddIpToDetailsTable.Designer.cs
+++ src/NoviSample.Persistence/Migrations/20191127231654_AddIpToDetailsTable.Designer.cs
@@ -4,14 +4,16 @@ using Kritikos.NoviSample.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Kritikos.NoviSample.Persistence.Migrations
 {
     [DbContext(typeof(NovibetDbContext))]
-    partial class NovibetDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191127231654_AddIpToDetailsTable")]
+    partial class AddIpToDetailsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/NoviSample.Services/Models/IpDetailResponse.cs
+++ src/NoviSample.Services/Models/IpDetailResponse.cs
@@ -19,6 +19,8 @@ namespace Kritikos.NoviSample.Services.Models
 		public byte[] RowVersion { get; set; }
 #pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
 
+		public string Ip { get; set; } = string.Empty;
+
 		public string City { get; set; } = string.Empty;
 
 		[JsonProperty(PropertyName = "country_name")]
