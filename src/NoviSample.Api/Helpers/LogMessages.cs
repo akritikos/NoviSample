e1e156e9996a7378475de41ede8fea8de3878eef
Alexandros Kritikos <akritikos@outlook.com>
2019-11-28 02:14:57 +0200

    :loud_sample: Adds more logging templates covering some of the corner cases

--- src/NoviSample.Api/Helpers/LogMessages.cs
+++ src/NoviSample.Api/Helpers/LogMessages.cs
@@ -5,5 +5,22 @@ namespace Kritikos.NoviSample.Api.Helpers
 		public const string UnhandledException = "An unhandled exception has occured! {Description}";
 
 		public const string MigrationsApplied = "Successfuly applied migrations: {@MigrationList}";
+
+		public const string RemovingIpFromCacheRequested = "Requested removal of {Ip} from cache!";
+
+		public const string RemovalOfNonExistingCacheItemRequested =
+			"Requested removal of {Ip} from cache, no such address in cache though!";
+
+		public const string RequestedInfoForIp = "Requested {Ip} info, cache hit!";
+
+		public const string RequestedInfoForIpNoCache = "Requested {Ip} info, cache miss!";
+
+		public const string RequestedInfoForIpNoPersistence = "Requested {Ip} info, database miss!";
+
+		public const string InvalidIpFormat = "Requested IP information for invalid ip format: {Ip}";
+
+		public const string InternalIp = "Requested IP information for private ip adress range: {Ip}";
+
+		public const string InvalidIpResponse = "Unable to serialize data to IpDetails: {Reason}";
 	}
 }
